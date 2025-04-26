55960c1b0f1ab4f70089b5fa44839d37cc83cc9d
Nikolay Terleev <unclepepper.dev@gmail.com>
2025-04-26 18:25:20 +0300

    feat: 26.04.2025 18:25:15

--- commit.sh
+++ commit.sh
@@ -3,6 +3,6 @@ current_time=$(date +"%d.%m.%Y %H:%M:%S")
 echo "Date: $current_time" >> log.txt
 sleep 5
 git add .
-git commit -m "feat: $current_time"
-sleep 5
+git commit -m "feat: $current_time" || exit 1
+
 ./push.sh
